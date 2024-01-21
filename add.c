f2e68484b846db74692ad420258f55d3c2982801
theNareshofficial <naresh@Naresh.naresh>
2024-01-21 13:31:05 -0800

    Addition

---
+++ add.c
@@ -0,0 +1,17 @@
+// This program is used to How to Add values 
+
+
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+
+	printf("Enter A value : "); 
+	scanf("%d", &a);		// Get A value
+	printf("Enter B value : "); 
+	scanf("%d", &b);		// Get B value
+	printf("A = %d \n",a);
+	printf("B = %d \n",b);
+	printf("Total = %d",a+b);	// Adding A and b value
+}
