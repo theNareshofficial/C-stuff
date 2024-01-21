4a54c29d77873b79f7fa1062cc31e3673db1ffce
Naresh <naresh@Naresh.naresh>
2024-01-21 13:25:14 -0800

    Ascii to char

---
+++ a2z.c
@@ -0,0 +1,13 @@
+// This program is used to print Alphabet small character ASCII value.
+
+
+#include <stdio.h>
+
+int main()
+
+{
+	for(int i = 'a';i < 'a';i++)
+	{
+		printf("%d \t=> %c\n",i,i);
+	}
+}
