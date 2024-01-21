1541ecadecb80b0d7e42ddd2232a840d56c463a0
Naresh <naresh@Naresh.naresh>
2024-01-21 13:20:00 -0800

    Hello World

---
+++ hello.c
@@ -0,0 +1,17 @@
+// This program is entry level for the programming Hello World!!! 
+
+/*
+include is library
+<stdio.h> is header file
+Eg:
+    std = Standard
+     io = input output
+     .h = header file     
+*/
+
+#include <stdio.h>
+
+int main()
+{
+        printf("Hello World!!!");       //Output : Hello world!!!
+}
