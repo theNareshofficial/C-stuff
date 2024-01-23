aec7df71169950c80cafbea148c40913fbc3ecfd
theNareshofficial <thenareshoffcial@gmail.com>
2024-01-23 10:31:42 -0800

    Datatype's

---
+++ datatype_size.c
@@ -0,0 +1,16 @@
+// This program is used for Datatype size 
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	printf("The 'Int' value is : %d\n", sizeof(int));
+	printf("The 'Char' value is : %d\n", sizeof(char));
+	printf("The 'Short' value is : %d\n", sizeof(short));
+	printf("The 'Float' value is : %d\n", sizeof(float));
+	printf("The 'Double' value is : %d\n", sizeof(double));
+	printf("The 'Long' value is : %d\n", sizeof(long));
+	printf("The 'Bool' value is : %d\n", sizeof(bool));
+}
