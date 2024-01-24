c9e6f11e04ef5e082d92c0bb66e3fdd0641c3d68
theNareshofficial <thenareshoffcial@gmail.com>
2024-01-24 09:49:30 -0800

    Datatype's

---
+++ data_type.c
@@ -0,0 +1,19 @@
+
+// This program is used for understand Datatype how to accept the data's.
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+
+	printf("[INT] value of : %i\n",a);
+	printf("[NUM] value of : %d\n",a);
+	printf("[OCTAL] value of : %o\n",a);
+	printf("[HEX] value of : %X\n",a);
+
+	printf("[CHAR] value of : %c\n",a);
+	printf("[ADDR] value of : %p\n",a);
+	printf("[CAPITAL] value of : %E\n",a);
+
+}
