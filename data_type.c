ecf1f191e60b5a51ef8e47b21a68a597e606615f
theNareshofficial <thenareshoffcial@gmail.com>
2024-01-24 09:50:23 -0800

    Different type of Datatypes

--- data_type.c
+++ data_type.c
@@ -11,7 +11,6 @@ int main()
 	printf("[NUM] value of : %d\n",a);
 	printf("[OCTAL] value of : %o\n",a);
 	printf("[HEX] value of : %X\n",a);
-
 	printf("[CHAR] value of : %c\n",a);
 	printf("[ADDR] value of : %p\n",a);
 	printf("[CAPITAL] value of : %E\n",a);
