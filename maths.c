a9d1a93d0ab2d2ca9ab59fad1958af4904e69240
theNareshofficial <thenareshoffcial@gmail.com>
2024-01-25 09:11:50 -0800

    math

---
+++ maths.c
@@ -0,0 +1,21 @@
+
+// This function is used to math functions 
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+        float bill = 3.4;
+        int a = 5;
+
+        //printf("Enter Your Amount : ");
+        //scanf("%f",&bill);
+
+        printf("Your Bill : %f\n",bill);
+       // printf("Your Bill : %f\n",ceil(bill));
+      //  printf("Your Bill : %f\n",floor(bill));
+        printf("Power function : %f\n",pow(3,2));
+        printf("Sqrt function : %f\n",sqrt(25.5));
+        printf("Log function : %f\n",log10(2));
+}
