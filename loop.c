22fe7c8b5c1dc9628c702927057af20f198b354f
theNareshofficial <thenareshoffcial@gmail.com>
2024-02-02 07:52:15 -0800

    for loop

---
+++ loop.c
@@ -0,0 +1,15 @@
+
+// This program is used to create for loop in 10 times
+
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+
+	for(i = 0; i < 11; i=i+1)
+	{
+		printf("%d.) Hello World\n",i);
+	}
+	return 0;
+}
